ecd9567a18916a27a76dd6f9af118d007b9d7ac4
Kavitha Thanigaivelan <kavithan98@gmail.com>
2018-08-05 11:14:10 -0700

    renamed

--- src/components/UserListPage/menu.js
+++ src/components/UserListPage/menu.js
@@ -1,22 +1,30 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import './UserPlan.css'
+import { Link } from 'react-router-dom';
+import './UserPlan.css';
 
-const Menu = (props) => {
-    return(
-              <div className="nav-main-menu">
-              <img src={require('../img/newTcLogo2.png')} className='logoImage1' alt='tclogo'/>
-              <nav>
-                <ul className='main-nav'>
-                    <li><Link to="/signout">Log Out</Link></li>
-                    <li><Link to="/userplan/existing">Existing Plan</Link></li>
-                    <li><Link to="/userplan/add">Add Plan</Link></li>
-                </ul>
-                </nav>
-              </div>
-
-    );
-}
-
+const Menu = props => {
+  return (
+    <div className="nav-main-menu">
+      <img
+        src={require('../img/newTcLogo2.png')}
+        className="logoImage1"
+        alt="tclogo"
+      />
+      <nav>
+        <ul className="main-nav">
+          <li>
+            <Link to="/signout">Log Out</Link>
+          </li>
+          <li>
+            <Link to="/userplan/existing">Existing Plan</Link>
+          </li>
+          <li>
+            <Link to="/userplan/add">Add Plan</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
 export default Menu;
