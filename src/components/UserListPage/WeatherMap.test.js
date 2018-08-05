614c310a32493b2867ba6f788ea357bf42a5010c
Kavitha Thanigaivelan <kavithan98@gmail.com>
2018-08-05 10:51:07 -0700

    changed some tests

--- src/components/UserListPage/WeatherMap.test.js
+++ src/components/UserListPage/WeatherMap.test.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import WeatherMap from './WeatherMap';
+import WeatherForm from './WeatherForm';
 
 describe('<WeatherMap />', () => {
   it('Renders without crashing', () => {
-    shallow(<WeatherMap />);
+    shallow(<WeatherForm />);
   });
 });
